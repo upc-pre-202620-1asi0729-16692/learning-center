f4d738804e0cb9db5b6f2a75317d4e9d8f58f841
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-24 10:45:41 -0500

    fix: remove unused import in main component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Layout } from './shared/presentation/components/layout/layout';
 
 @Component({
-  imports: [RouterOutlet, Layout],
+  imports: [Layout],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
