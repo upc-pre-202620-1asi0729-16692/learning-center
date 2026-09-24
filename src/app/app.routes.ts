1a2f1f1de98b9705d44e05fc12036ea28f93d3c2
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-24 09:59:10 -0500

    feat(home): add Home view with navigation route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/presentation/views/home/home';
+const baseTitle = 'ACME Learning Center';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home', component: Home, title: `${baseTitle} - Home`},
+];
