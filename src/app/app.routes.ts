de90a424233aa65ec325cfdd3159e39cc2932b8f
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-24 10:10:07 -0500

    feat(about): add About Us view with navigation route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './shared/presentation/views/home/home';
 const baseTitle = 'ACME Learning Center';
+const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
 
 export const routes: Routes = [
   {path:'home', component: Home, title: `${baseTitle} - Home`},
+  {path:'about', loadComponent: about, title: `${baseTitle} - About`}
 ];
